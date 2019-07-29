bf64e250cb9b3afa7baa4d141f18aec00d414b29
Stacey-31 <53440572+Stacey-31@users.noreply.github.com>
2019-07-29 13:55:45 -0400

    Add files via upload

---
+++ Day1stacey.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import plotly
+print("My name is Stacey Acheampong")
+print(5*2)
+print(5/2)
+print(5-2)      
+print(5**2)
+print((8/9)*(3))
